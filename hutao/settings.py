729aebdb8d76d26662ede72ee70151e66f1ce560
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-18 23:48:35 +0700

    update settings

--- hutao/settings.py
+++ hutao/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "homa.apps.HomaConfig",
+    "commons.apps.CommonsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
