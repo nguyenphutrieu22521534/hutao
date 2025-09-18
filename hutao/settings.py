e27c22477aa759150272d65d20e6e27c7315c131
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-19 00:20:10 +0700

    create company

--- hutao/settings.py
+++ hutao/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "applications.homa.apps.HomaConfig",
-    "applications.commons.apps.CommonsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
