a448f37537cca805a774fddca5b8b3f73f957451
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-19 16:16:32 +0700

    migrate model company

--- hutao/settings.py
+++ hutao/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from applications import company
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "applications.company.apps.CompanyConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
