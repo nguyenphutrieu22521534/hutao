297830293a844f334dd57e4d837487144b1455c0
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-23 13:46:20 +0700

    delete

--- applications/commons/models.py
+++ applications/commons/models.py
@@ -1,14 +1,2 @@
 from django.db import models
 
-class Article(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
-    content = models.TextField()
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['title']),
-            models.Index(fields=['pub_date']),
-            models.Index(fields=['title', 'pub_date']),
-            models.Index(fields=['-pub_date'], name='recent_articles_idx'),
-        ]
