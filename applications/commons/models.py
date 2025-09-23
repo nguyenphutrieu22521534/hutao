733fecb1ed4d19a52d7b0cd10663e2c4c03735ee
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-23 09:58:46 +0700

    model index

--- applications/commons/models.py
+++ applications/commons/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    content = models.TextField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['pub_date']),
+            models.Index(fields=['title', 'pub_date']),
+            models.Index(fields=['-pub_date'], name='recent_articles_idx'),
+        ]
