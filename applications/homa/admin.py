33304288bbadeeb7fe469c3433c1fcaf1a660282
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-26 14:31:03 +0700

    admin site homa

--- applications/homa/admin.py
+++ applications/homa/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
 
+admin.site.register(models.Apartment)
+admin.site.register(models.Resident)
+admin.site.register(models.ElectricityIndicator)
+admin.site.register(models.WaterIndicator)
+admin.site.register(models.Bill)
