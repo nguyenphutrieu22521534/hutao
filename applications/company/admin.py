5df4b09cfffdd05e585585c46deafde85fb4dd37
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-09-26 09:48:25 +0700

    admin site register company

--- applications/company/admin.py
+++ applications/company/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Company)
+admin.site.register(models.Department)
+admin.site.register(models.Employee)
+admin.site.register(models.Customer)
+admin.site.register(models.Contract)
+
