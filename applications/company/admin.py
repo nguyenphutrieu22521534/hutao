4b3669e4cfe8fc1abb67ed40a40858c3be503e7a
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-10-01 16:50:47 +0700

    format money

--- applications/company/admin.py
+++ applications/company/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
+from . import models
 
+def get_all_field_names(model):
+    return [field.name for field in model._meta.fields]
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = get_all_field_names(models.Company)
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = get_all_field_names(models.Department)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = get_all_field_names(models.Employee)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = get_all_field_names(models.Customer)
+    search_fields = ["name"]
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = get_all_field_names(models.Contract)
+
+admin.site.register(models.Company, CompanyAdmin)
+admin.site.register(models.Department, DepartmentAdmin)
+admin.site.register(models.Employee, EmployeeAdmin)
+admin.site.register(models.Customer, CustomerAdmin)
+admin.site.register(models.Contract, ContractAdmin)
