017958d1f0d7d7f7a1d99cb64e8f0465883d95b8
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-10-24 07:19:41 +0700

    setup oauth gg

--- hutao/settings.py
+++ hutao/settings.py
@@ -63,8 +63,6 @@ ROOT_URLCONF = 'hutao.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
